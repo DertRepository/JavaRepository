7bdaeec7f17363e93ee8680e0363122655cba251
Monkey <Dert.thatch@gmail.com>
2022-07-15 21:19:38 +0700

    TEST

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,10 @@
 public class Main {
 //    Check
     static void argsPrinting(String msg, int... args) {
-        for (int argsStore : args) {
-            System.out.println(msg + "args[" + argsStore + "]" + argsStore * argsStore);
-        }
+
     }
 
     public static void main(String[] args) {
-        argsPrinting("Hello World", 1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
     }
 }
