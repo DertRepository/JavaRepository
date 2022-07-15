568c99ba76529bec70cb733bf0d66bd70c516cb6
Monkey <Dert.thatch@gmail.com>
2022-07-15 21:08:15 +0700

    05--Arugment printing loop

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
-    public static void main(String[] args) {
+    static void argsPrinting(String msg, int... args) {
+        for (int argsStore : args) {
+            System.out.println(msg + "args[" + argsStore + "]" + argsStore * argsStore);
+        }
+    }
 
+    public static void main(String[] args) {
+        argsPrinting("Hello World", 1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
     }
 }
